bc0a436664bb32dee750209898d247dfde145e9b
GiftMathotsi <giftnyiko234@gmail.com>
2023-10-06 14:31:45 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - the beginning
+ * Return: is 0
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			if ((c != 'q' && c != 'e') && c <= 'z')
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+		return (0);
+}
